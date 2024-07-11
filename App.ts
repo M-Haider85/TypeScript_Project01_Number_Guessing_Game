e6b671b049e30dd5038308122a8b24b7b0427fd1
Muhammad Haider <125929028+M-Haider85@users.noreply.github.com>
2024-07-11 15:14:00 +0500

    Add files via upload

---
+++ App.ts
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let roundNumber: number = Math.floor((Math.random() * 10) + 1);
+
+let userInput = await inquirer.prompt({
+    type:"number",
+    name:"guessedNumber",
+    message:"Guess a number between 1 - 10"
+})
+
+if(userInput.guessedNumber === roundNumber){
+    console.log(`Congratulations! You guessed it correctly!`);
+}
+else{
+    console.log(`Sorry! Try again. Correct answer is: ${roundNumber}`);
+}
